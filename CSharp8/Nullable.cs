d03ffc3411cc110467f421811c652931729d5b29
cartermp <pcarter@fastmail.com>
2019-11-04 10:53:52 -0500

    Initial commit

---
+++ CSharp8/Nullable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Enable nullability analysis for the whole file.
+// For a proejct, set '<Nullable>enable</Nullable>' in the project file.
+#nullable enable
+
+namespace CSharp8
+{
+    // This is 100% ad-hoc; nothing is prepared :)
+    public static class NullableWithStrings
+    {
+        /// <summary>
+        /// Unsafely "dot" into a nullable reference type and observe the warning.
+        /// </summary>
+        public static int StringLengthUnsafe(string? theString) => theString.Length;
+
+        /// <summary>
+        /// Safely "dot" into a nullable reference type. Flow analysis tells us it's safe.
+        /// </summary>
+        public static int StringLengthSafe(string? theString) =>
+            string.IsNullOrWhiteSpace(theString) ? 0 : theString.Length;
+    }
+}
---
+++ CSharp8/StaticLocalFunctions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8
+{
+    class StaticLocalFunctions
+    {
+    }
+}
